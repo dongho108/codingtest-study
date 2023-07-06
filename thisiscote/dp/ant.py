c169a3d19655e1fe7a65b997fda3379b7d58c5fc
dongho <dongho108@naver.com>
2023-07-06 16:23:53 +0900

    max(i-2까지 최대값 + 현재값, i-1까지 최대값)

---
+++ thisiscote/dp/ant.py
@@ -0,0 +1,15 @@
+n = int(input())
+k = list(map(int, input().split()))
+
+dp = [0] * n
+dp[0] = k[0]
+dp[1] = max(k[0], k[1])
+for i in range(2, n):
+    dp[i] = max(dp[i-2] + k[i], dp[i-1])
+
+print(max(dp))
+print(dp)
+"""
+4
+1 3 1 5
+"""
